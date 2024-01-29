6baaf68d95198fdda0fc0794becc08a91e72a817
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-01-29 10:57:23 -0500

    Added utils module

---
+++ utils.py
@@ -0,0 +1,15 @@
+class clr:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+def cprint(text: str, color: str):
+    """Prints out the given text with the provided color."""
+    print(f"{color}{text}{clr.ENDC}")
