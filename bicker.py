3ed052e44045f388ce80abb66870d46c25e151db
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-01-30 10:40:38 -0500

    Added rank and percentiles to output CSV

--- bicker.py
+++ bicker.py
@@ -128,6 +128,14 @@ def main():
     )
 
     output = output.merge(bickeree_stats, on="bickeree_number")
+
+    # Rank the bickerees by their weighted score, where 1 is the best and N is the worst
+    output["rank"] = (
+        output["weighted_score"].rank(ascending=False, method="min").astype(int)
+    )
+    # Calculate percentiles, where 0.99 means the bickeree is in the top 1% of all bickerees,
+    # and 0.01 means the bickeree is in the bottom 1% of all bickerees
+    output["percentile"] = output["weighted_score"].rank(pct=True, ascending=True)
     output.to_csv("bicker_output.csv")
 
 
